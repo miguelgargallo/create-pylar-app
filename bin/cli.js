1c1828db774f78328d509a85b19a02f43d773d1d
miguelgargallo <5947268+miguelgargallo@users.noreply.github.com>
2022-10-25 12:02:31 +0200

    0.0.2 Fixed cli.js 16

---
+++ bin/cli.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+
+const { execSync } = require('child_process');
+
+const runCommand = command => {
+    try {
+        execSync('${command}', { stdio: 'inherit' })
+    } catch (e) {
+        console.error('Failed to execute ${command}', e);
+        return false;
+    }
+    return true;
+}
+
+const repoName = process.argv[2];
+const gitCheckoutCommand = 'git clone --depth=1 https://github.com/miguelgargallo/create-pylar-app ${repoName}';
+
+console.log('Cloning repository with name ${repoName}');
+const gitCheckout = runCommand(gitCheckout);
+if (!gitCheckout) process.exit (code: -1);
+
+console.log("Congratulations! You're ready to go!");
+console.log('cd ${repoName} && ./runApp');
